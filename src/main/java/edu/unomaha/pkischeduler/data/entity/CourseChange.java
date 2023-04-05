72e2d4a3ca0254075ff39088db3694b4163b13df
Dejan Dragos <ddragos@unomaha.edu>
2023-04-05 03:17:23 -0500

    fix

--- src/main/java/edu/unomaha/pkischeduler/data/entity/CourseChange.java
+++ src/main/java/edu/unomaha/pkischeduler/data/entity/CourseChange.java
@@ -11,30 +11,37 @@ public class CourseChange extends AbstractEntity {
         @Transient
         protected static final  DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-        @NotEmpty
-        protected LocalDateTime dateTime;
+        @Transient
+        protected LocalDateTime dateT;
+
+        protected String dateTime;
+
 
         @Transient
         protected  Course before;
         @Transient
         protected  Course after;
-        protected  boolean changeToBeforeOrAfter =true;
+
+        @Transient
+        protected  transient boolean changeToBeforeOrAfter = true;
 
         /**
          * Store the difference between the before and after course
+         * Example: Course{courseCode='BMI 8080', courseTitle='SEMINAR IN BIOMEDICAL INFO', sectionType='Seminar' [from=Seminar42],....
          */
-        @Column(length = 1024)
+        @NotEmpty
+        @Column(length = 1024)  // Database is using VARCHAR so space is not wasted...
         protected String changeCourse ="";
 
 
         public void setBefore(Course before) {
-                dateTime = LocalDateTime.now();
+                dateT = LocalDateTime.now();
                 this.before = before;
                 changeToBeforeOrAfter = true;
         }
 
         public void setAfter(Course after) {
-                dateTime = LocalDateTime.now();
+                dateT = LocalDateTime.now();
                 this.after = after;
                 changeToBeforeOrAfter = true;
         }
@@ -44,18 +51,19 @@ public class CourseChange extends AbstractEntity {
         @Override
         public String toString(){
                 beforePersist();
-                return dateTimeFormatter.format( dateTime )  + " " + changeCourse;
+                return dateTimeFormatter.format(dateT)  + " " + changeCourse;
         }
 
         /**
          * Function is called automatically to make sure that all variables are in order before the object is persisted
          */
-//        @PrePersist
+        @PrePersist
         public void beforePersist() {
                 if (changeToBeforeOrAfter) {
                         changeCourse = before.getChangesFrom(after);
                         changeToBeforeOrAfter = false;
                 }
+                dateTime = dateTimeFormatter.format(dateT);
         }
 
 
