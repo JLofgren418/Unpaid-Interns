6aee4ea1d46bcbec4c60c163e8854ef90242ccdc
Brady Kelley <bakelley@unomaha.edu>
2023-04-10 10:53:34 -0500

    Add Doc comments for JavaDoc

--- src/main/java/edu/unomaha/pkischeduler/data/service/CRIService.java
+++ src/main/java/edu/unomaha/pkischeduler/data/service/CRIService.java
@@ -13,14 +13,34 @@ import java.util.Collection;
 import java.util.List;
 
 
-
+/**
+ * Service provider which allows the application to access
+ * the repositories for Courses, Rooms, and Instructors (CRI)
+ */
 @Service
 public class CRIService implements CrudListener<Course> {
 
+    /**
+     * The course repository interface.
+     */
     private final CourseRepository courseRepository;
+
+    /**
+     * The room repository interface.
+     */
     private final RoomRepository roomRepository;
+
+    /**
+     * The instructor repository interface.
+     */
     private final InstructorRepository instructorRepository;
 
+    /**
+     * Default constructor.
+     * @param courseRepository The course repository interface.
+     * @param roomRepository The room repository interface.
+     * @param instructorRepository The instructor repository interface.
+     */
     public CRIService(CourseRepository courseRepository,
                       RoomRepository roomRepository,
                       InstructorRepository instructorRepository) {
@@ -30,6 +50,13 @@ public class CRIService implements CrudListener<Course> {
     }
 
 
+    /**
+     * Allows courses to be filtered by user input.
+     * This function calls the courseRepository to execute a query.
+     * The query executed returns a list of courses that match user input.
+     * @param stringFilter The user input.
+     * @return A list of courses that match the user input.
+     */
     public List<Course> filterCourses(String stringFilter) {
         if (stringFilter == null || stringFilter.isEmpty()) {
             return courseRepository.findAll();
@@ -38,6 +65,13 @@ public class CRIService implements CrudListener<Course> {
         }
     }
 
+    /**
+     * Allows rooms to be filtered by user input.
+     * This function calls the roomRepository to execute a query.
+     * The query executed returns a list of rooms that match user input.
+     * @param stringFilter The user input.
+     * @return A list of rooms that match the user input.
+     */
     public List<Room> filterRooms(String stringFilter) {
         if (stringFilter == null || stringFilter.isEmpty()) {
             return roomRepository.findAll();
@@ -47,54 +81,99 @@ public class CRIService implements CrudListener<Course> {
     }
 
 
-
+    /**
+     * This function calls the roomRepository
+     * to return all rooms in the database.
+     * @return A list of all rooms in the database.
+     */
     public List<Room> getAllRooms() {
 
         return roomRepository.findAll();
     }
 
+
+    /**
+     * This function calls the instructorRepository
+     * to return all instructors in the database.
+     * @return A list of all instructors in the database.
+     */
     public List<Instructor> getAllInstructors() {
 
         return instructorRepository.findAll();
     }
 
+    /**
+     * This function calls the courseRepository
+     * to return all courses in the database.
+     * @return A list of all courses in the database.
+     */
     public List<Course> getAllCourses() {
 
         return courseRepository.findAll();
     }
 
+    /**
+     * This function calls the courseRepository
+     * Used specifically for the CRUD grid in EditView
+     * to return all courses in the database.
+     * @return A collection of all courses in the database.
+     */
     @Override
     public Collection<Course> findAll() {
 
         return courseRepository.findAll();
     }
 
+    /**
+     * This function calls the courseRepository
+     * Used specifically for the CRUD grid in EditView
+     * to add a courses to the database.
+     * @return A call to the courseRepository to save a course.
+     */
     @Override
     public Course add(Course course) {
         return courseRepository.save(course);
     }
 
+    /**
+     * This function calls the courseRepository
+     * Used specifically for the CRUD grid in EditView
+     * to update a courses in the database.
+     * @return A call to the courseRepository to update a course.
+     */
     @Override
     public Course update(Course course) {
 
         return courseRepository.save(course);
     }
 
+    /**
+     * This function calls the courseRepository
+     * Used specifically for the CRUD grid in EditView
+     * to delete a courses in the database.
+     */
     @Override
     public void delete(Course course) {
         courseRepository.delete(course);
     }
 
 
+    /**
+     * This function calls the roomRepository
+     * to add a room to the database.
+     * @param room the room to be added.
+     * @return A call to the roomRepository to save the room to the database.
+     */
     public Room addRoom(Room room)
     {
         return roomRepository.save(room);
     }
 
     /**
-     * Allows an instructor to be added to the databse.
-     * @param instructor
-     * @return
+     * This function calls the instructorRepository
+     * to add an instructor to the database.
+     * @param instructor instructor The instructor to be added
+     * @return a call to the instructorRepository to save the instructor.
      */
     public Instructor addInstructor(Instructor instructor)
     {
@@ -104,7 +183,7 @@ public class CRIService implements CrudListener<Course> {
     /**
      * Provides the default room from the database.
      * All courses are initially set to this room.
-     * @return The default room.
+     * @return A call to the roomRepository which gives the default room at ID=0
      */
     public Room getDefaultRoom()
     {
@@ -138,8 +217,6 @@ public class CRIService implements CrudListener<Course> {
         courseRepository.deleteAll();
         instructorRepository.deleteAll();
     }
-
-
 
 
 }
